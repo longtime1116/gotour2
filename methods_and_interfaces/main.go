cfb72fb659d7b0812cc0da67644f463eddbe5b85
longtime1116 <longtime1116@gmail.com>
2024-08-21 00:58:36 +0900

    on going

--- methods_and_interfaces/main.go
+++ methods_and_interfaces/main.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+type Abser interface {
+	Abs() float64
+}
+
 type Vertex struct {
 	X, Y float64
 }
@@ -12,13 +16,18 @@ type Vertex struct {
 type MyFloat64 float64
 
 // receiver 引数があるだけで、実際のところただの関数。
-func (v Vertex) Abs() float64 {
+func (v *Vertex) Abs() float64 {
+	// このように、nilのケースに対処する処理を素直に書くのが一般的
+	if v == nil {
+		return 0
+	}
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 // ポインタのレシーバーにする理由：
 //  1. 構造体の持つvalueを変更できる
 //  2. レシーバーの値をコピーしないので、メモリ効率が高まる
+//  3. 特定のinterfaceを満たすという性質に合わせるために、ポインタにしておきたくなる
 func (v *Vertex) Scale(n float64) {
 	v.X *= n
 	v.Y *= n
@@ -27,11 +36,11 @@ func (v *Vertex) Scale(n float64) {
 	// (*v).Y *= n
 }
 
-func (x MyFloat64) Abs() MyFloat64 {
+func (x MyFloat64) Abs() float64 {
 	if x < 0 {
-		return -x
+		return float64(-x)
 	}
-	return x
+	return float64(x)
 }
 
 func main() {
@@ -44,4 +53,25 @@ func main() {
 	// もちろんこうやって書くこともできる
 	// (&v1).Scale(2)
 	fmt.Println(v1)
+
+	// Abser インタフェースを満たすもののみに代入可能
+	// ここで、a は Abser インタフェースを満たす何らかのものであることを示す
+	var a Abser
+	a = &v1
+	a = x1
+	fmt.Println(a)
+	// a = v1 // エラーになる。
+	// こんなふうにもできる
+	var b Abser = &Vertex{1, 2}
+	fmt.Println(b)
+
+	var p *Vertex
+	b = p
+	fmt.Printf("%v, %T\n", b, b)
+	fmt.Printf("%v, %T\n", p, p)
+	fmt.Println(b.Abs())
+
+	var c Abser
+	fmt.Printf("%v, %T\n", c, c)
+	// c.Abs() // SEGV
 }
